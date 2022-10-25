b579f098878fa02c61fd99ae8099cb1f70fe284f
Kayxzy <106707683+Kayxzy@users.noreply.github.com>
2022-10-25 10:33:53 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,8 +1,6 @@
 #(©)Codexbotz
 
-from pyrogram import __version__
 from bot import Bot
-from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Bot.on_callback_query()
@@ -10,7 +8,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ </b>",
+            text = f"<b>○ Creator : Owner </b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
